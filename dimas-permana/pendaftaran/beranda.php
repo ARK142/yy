7c47c1c66f05936d57a4bd6fc897267da6c8f97f
ARK142 <81946069+ARK142@users.noreply.github.com>
2022-01-25 09:09:50 +0700

    Add files via upload

---
+++ dimas-permana/pendaftaran/beranda.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="asset/CSS/bootstrap-reboot.min.css">
+    <!-- owlcarousel CSS -->
+    <link rel="stylesheet" href="asset/CSS/owl.carousel.min.css">
+    <!-- fontawsome CSS -->
+    <link rel="stylesheet" href="asset/fontawesome-free-5.15.4-web/css/all.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="asset/CSS/sytle.css">
+</head>
+
+<body>
+    <section id="topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <ul class="top-contact">
+                        <li><a href=""><i class="fas fa-phone"></i>0877 1117 7442</a></li>
+                        <li><a href=""><i class="fas fa-enelope"></i>smkit.pesat@gmail.com</a></li>
+                        <li><a href=""><i class="fas fa-bullhorn"></i>PPDB TA 2022/2023 Telah
+                                dibuka!!!</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <ul class="sosmed">
+                        <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                        <li><a href=""><i class="fab fa-youtube"></i></a></li>
+                        <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="brand">
+                        <img src="img/download (1).png" alt="">
+                    </div> <!-- md8-->
+                </div>
+                <div class="col-md-4 pembungkus-searchbox">
+                    <div class="searchbox">
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-utama my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div> <!-- md4-->
+            </div> <!-- row-->
+        </div> <!-- container-->
+    </header>
+    <!-- section tool-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-biru">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"> Menu</span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link active " href="#">Beranda <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="button">
+                            Profil
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Kurikulum IT EXPRO</a>
+                            <a class="dropdown-item" href="#">Civitas Akademika</a>
+                            <a class="dropdown-item" href="#">Galeri</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Berita</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Event</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontak</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">pendaftaran</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <script src="asset/JS/jquery-3.3.1.slim.min.js"></script>
+    <script src="asset/JS/bootstrap.min.js"></script>
+    <script src="asset/JS/owl.carousel.min.js"></script>
+    <script src="asset/JS/popper.min.js"></script>
+    <script src="asset/JS/bootstrap.bundle.min.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
